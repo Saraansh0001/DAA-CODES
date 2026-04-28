38bf2f4e49eaab9fc35acf669c17f9279ce321ea
Saraansh <gandhisaraansh@gmail.com>
2026-04-28 22:39:23 +0530

    code updated

--- L1/heapSort.java
+++ L1/heapSort.java
@@ -25,21 +25,23 @@ public class HeapSort {
     }
 
     public static void main(String[] args) {
-        int arr[] = {4, 10, 3, 5, 1};
-        int n = arr.length;
+        int[] arr = { 1 , 2, 98 , 56 ,23 ,45, 78 ,4,2 666};
 
-        for(int i = n/2 - 1; i >= 0; i--)
-            heapify(arr, n, i);
+        int n = arr.length ;
 
-        for(int i = n-1; i > 0; i--) {
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
+        for ( int i = n/2-1 ; i>= 0 ; i++) heapify( arr , n , i);
 
-            heapify(arr, i, 0);
+        for ( int i = n-1 ; i >0 ; i --){
+          int temp = arr[0];
+          arr[0] = arr[i] ;
+          arr[i] = temp ;
+
+          heapify ( arr , i , 0);
         }
 
-        for(int i : arr)
-            System.out.print(i + " ");
+        System.out.println("Sorted  array is as follows :- ");
+
+        for( int i = 0 ; i < n ; i ++) System.out.print(arr[i] + " ");
+
     }
 }
