71f048c20d2b30423e0276bd9038960bdb509a04
Saraansh <gandhisaraansh@gmail.com>
2026-04-28 21:41:21 +0530

    code updated

--- selectionSort.java
+++ selectionSort.java
@@ -1,27 +1,31 @@
-public class selectionSort {
+import java.util.*;
 
-  public static void main(String[] args) {
-    
-    int[] arr = {64, 25, 12, 22, 11};
-    int n = arr.length;
-    for ( int i = 0 ; i < n ; i++){
-      int minIndex = i ;
-      for ( int  j = i+1 ; j < n ; j ++){
-        if ( arr[j] < arr[minIndex]){
-          minIndex = j ;
+class solution{
+    public static void main( String[] args ){
+        
+        int[] arr = { 11 , 56 , 89, 7891 , 56 , 45
+        , 74 , 86976  , 86 };
+        
+        int n = arr.length ;
+        
+        for ( int i = 0 ; i < n ; i++){
+            int minIndex = i;
+            for ( int j = i+1 ; j<n ; j ++){
+                if ( arr[j] < arr[minIndex]){
+                    minIndex  = j ;
+                }
+            }
+            
+            int temp = arr[i];
+            arr[i]=  arr[minIndex] ;
+            arr[minIndex] = temp ;
         }
-      }
-      int temp = arr[i] ;
-      arr[i] = arr[minIndex] ;
-      arr[minIndex] = temp ;
+        
+        System.out.println("Sorted array: ");
+        
+        for ( int i = 0 ; i < n ; i++){
+          System.out.print(arr[i] + " ");
+        }
+    
     }
-
-    System.out.println("Sorted array: ");
-    for ( int i = 0 ; i < n ; i++){
-      System.out.print(arr[i] + " ");
-    }
-
-  }
-  
 }
-// shortest ko pick krke first me rakhna hai
