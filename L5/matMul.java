52a395823c36c23fba419a8f695af25cc38d2247
Saraansh <gandhisaraansh@gmail.com>
2026-04-29 03:11:35 +0530

    code updated

--- L5/matMul.java
+++ L5/matMul.java
@@ -1,29 +1,30 @@
-import java.util.*;
+import java.util.* ;
 
-public class MCM {
-    public static int matrixChain(int[] arr) {
-        int n = arr.length;
-        int[][] dp = new int[n][n];
-
-        for (int len = 2; len < n; len++) {
-            for (int i = 1; i < n - len + 1; i++) {
-                int j = i + len - 1;
-                dp[i][j] = Integer.MAX_VALUE;
-
-                for (int k = i; k < j; k++) {
-                    int cost = dp[i][k] + dp[k + 1][j]
-                            + arr[i - 1] * arr[k] * arr[j];
-
-                    if (cost < dp[i][j])
-                        dp[i][j] = cost;
+public class Main {
+    public static int mcm( int[] arr ){
+        int n  = arr.length ;
+        int[][] dp = new int [n][n];
+        int len , i , j , k ;
+        
+        for( len = 2 ; len < n ; len++){
+            for ( i = 1 ; i < n-len+1 ; i++){
+                j = len+i-1;
+                dp[i][j] = Integer.MAX_VALUE ;
+                for ( k = i ; k<j ; k++){
+                    int cost = dp[i][k] + dp[k+1][j] + arr[i-1]*arr[j]*arr[k];
+                    if ( cost < dp[i][j]) dp[i][j] = cost ;
                 }
             }
         }
-        return dp[1][n - 1];
+        
+        return dp[1][n-1];
     }
-
-    public static void main(String[] args) {
-        int[] arr = {12, 6, 4, 9, 20, 50, 8, 5, 30, 10};
-        System.out.println("Minimum cost: " + matrixChain(arr));
+    
+    public static void main( String[] args ){
+        int[] arr = { 4,5,7,8,9,6,3,2,12,4,9};
+        
+        System.out.println("Minimum no. of multiplication is :- " 
+                            + mcm(arr));
     }
+    
 }
