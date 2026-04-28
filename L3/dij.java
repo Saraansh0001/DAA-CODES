b8e3cc316b0cfd6bdf3f18c16df60632321af8de
Saraansh <gandhisaraansh@gmail.com>
2026-04-29 01:42:07 +0530

    code updated

---
+++ L3/dij.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class Dijkstra {
+
+    static final int V = 5;
+
+    int minDistance(int dist[], boolean visited[]) {
+        int min = Integer.MAX_VALUE, minIndex = -1;
+
+        for(int i = 0; i < V; i++) {
+            if(!visited[i] && dist[i] < min) {
+                min = dist[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    void dijkstra(int graph[][], int src) {
+
+        int dist[] = new int[V];
+        boolean visited[] = new boolean[V];
+
+        for(int i = 0; i < V; i++) {
+            dist[i] = Integer.MAX_VALUE;
+            visited[i] = false;
+        }
+
+        dist[src] = 0;
+
+        for(int count = 0; count < V-1; count++) {
+            int u = minDistance(dist, visited);
+            visited[u] = true;
+
+            for(int v = 0; v < V; v++) {
+                if(!visited[v] && graph[u][v] != 0 &&
+                   dist[u] != Integer.MAX_VALUE &&
+                   dist[u] + graph[u][v] < dist[v]) {
+
+                    dist[v] = dist[u] + graph[u][v];
+                }
+            }
+        }
+
+        System.out.println("Vertex \t Distance");
+        for(int i = 0; i < V; i++) {
+            System.out.println(i + " \t " + dist[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int graph[][] = {
+            {0, 10, 0, 30, 100},
+            {10, 0, 50, 0, 0},
+            {0, 50, 0, 20, 10},
+            {30, 0, 20, 0, 60},
+            {100, 0, 10, 60, 0}
+        };
+
+        Dijkstra d = new Dijkstra();
+        d.dijkstra(graph, 0); // source = 0
+    }
+}
